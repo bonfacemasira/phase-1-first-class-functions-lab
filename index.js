2f327dc8989eb19de4170482f759f9924d65b65c
Bonface Masira <bonfacemasira@gmail.com>
2022-06-08 15:19:53 +0300

    DOne assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(drivers){
+    const firstTwo = [...drivers];
+    return firstTwo.splice(0,2);
+}
+
+const returnLastTwoDrivers = function(drivers){
+    const lastTwo = [...drivers];
+    return lastTwo.splice(-2);
+}
+
+const selectingDrivers = [
+    returnFirstTwoDrivers, returnLastTwoDrivers
+];
+
+function createFareMultiplier(fare) {
+    return function(multiplier){
+        return fare * multiplier;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, returnLastTwoDrivers) {
+    return returnLastTwoDrivers(drivers);
+}
